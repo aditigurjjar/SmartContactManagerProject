44f4ce71e188a79a53eb2d7738dbd70f28609a6c
Aditi Gurjar <gurjaraditi5@gmail.com>
2024-12-24 23:33:40 +0530

    add tables in db

---
+++ src/main/java/com/scm/SmartContactManager/entities/User.java
@@ -0,0 +1,47 @@
+package com.scm.SmartContactManager.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="user")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private String userId;
+    @Column(name="user_name",nullable = false)
+    private String name;
+    @Column(unique = true,nullable = false)
+    private String email;
+    private String password;
+    @Column(length=10000)
+    private String about;
+    @Column(length=10000)
+    private String profilePic;
+    private boolean enabled=false;
+    private boolean emailVerified=false;
+    private boolean phoneVerified=false;
+    
+    // Self,google,facebook,twitter,linkedin , github
+    private providers provider=providers.SELF;
+    private String providerUserId;
+
+    @OneToMany(mappedBy = "user")
+    private List<Contact> contacts=new ArrayList<>();
+}
---
+++ src/main/java/com/scm/SmartContactManager/entities/providers.java
@@ -0,0 +1,6 @@
+package com.scm.SmartContactManager.entities;
+
+public enum providers {
+
+    SELF,GOOGLE,GITHUB
+}
