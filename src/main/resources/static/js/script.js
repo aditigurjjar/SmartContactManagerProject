8f365575efc44554993d03cd0dbb1825e7a77564
Aditi Gurjar <gurjaraditi5@gmail.com>
2024-12-24 17:53:36 +0530

    theme

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,24 @@
 console.log("Script loadded");
+
+let currentTheme =getTheme();
+
+//TODO
+function changeTheme()
+{
+
+}
+
+//set theme to localstorage
+
+function setTheme(theme)
+{
+    localStorage.setTheme("theme", theme);
+}
+
+//get theme from localstorage
+function getTheme()
+{
+    let theme=localStorage.getItem("theme");
+    if(theme)return theme;
+    else return "light";
+}
